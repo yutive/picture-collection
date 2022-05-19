dd9eb4295c6755f92765126710998a85fb08421b
laurin <laurin.schwander@outlook.com>
2022-05-19 15:58:30 +0200

    Made a fiel uploadform

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpg', 'image/jpeg']
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('')
+        } else {
+            setFile(null);
+            setError('Please select an image file (png, jpg, jpeg)')
+        }
+    }
+
     return (
+
         <form>
-            <input type='file'/>
-        </form>)
+            <input type='file' onChange={changeHandler}/>
+            <div className='output'>
+                {error && <div>{error}</div>}
+                {file && <div>{file.name}</div>}
+            </div>
+        </form>
+
+    )
+
 }
 
 export default UploadForm;
