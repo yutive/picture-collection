bb4962486392f75234ab1aa02b664562638d79e0
laurin <laurin.schwander@outlook.com>
2022-05-20 16:01:07 +0200

    made a tailwind button design

--- src/components/PictureGrid.js
+++ src/components/PictureGrid.js
@@ -9,7 +9,7 @@ const PictureGrid = () => {
     return (
         <div className="img-grid">
             images
-            <button onClick={handleClick}>
+            <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
                 List all songs
             </button>
 
