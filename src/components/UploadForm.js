2ea34afbfb0abf0e4361bd89f3bda306b29ce874
laurin <laurin.schwander@outlook.com>
2022-05-25 13:42:02 +0200

    implemented multiple picture upload

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -3,38 +3,45 @@ import {ref, uploadBytes} from "firebase/storage";
 import {storage} from "../firebase/config";
 
 
-
 const UploadForm = () => {
     const [file, setFile] = useState(null);
     const [error, setError] = useState(null);
 
     const types = ['image/png', 'image/jpg', 'image/jpeg']
 
-    function uploadPic(file) {
-        let uuid = crypto.randomUUID();
+    function uploadPicture(file) {
+        let uuid = crypto.randomUUID()
         const storageRef = ref(storage, uuid);
         uploadBytes(storageRef, file).then((snap) => {
         });
     }
 
     const changeHandler = (e) => {
-        let selected = e.target.files[0];
+        let count = 0;
+        let selected = e.target.files;
 
+        for (let i = 0; i < selected.length; i++) {
+            let file = selected[i];
 
-        if (selected && types.includes(selected.type)) {
-            let file = selected
-            setError('')
-            uploadPic(file)
-        } else {
-            setFile(null);
-            setError('Please select an image file (png, jpg, jpeg)')
+            if (file && types.includes(file.type)) {
+                setError('')
+                uploadPicture(file)
+                console.log("huso")
+            } else {
+                setFile(null);
+                count++;
+                if (i + 1 === selected.length){
+                    setError(count+' have failed in cause of wrong format')
+                }
+            }
         }
+
     }
 
     return (
 
         <form>
-            <input type='file' onChange={changeHandler}/>
+            <input type='file' multiple onChange={changeHandler}/>
             <div className='output'>
                 {error && <div>{error}</div>}
                 {file && <div>{file.name}</div>}
