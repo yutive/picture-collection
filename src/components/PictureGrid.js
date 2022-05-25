c3724c0fdefa322ed7b47ecdf7e4113eef2867c6
laurin <laurin.schwander@outlook.com>
2022-05-25 13:45:46 +0200

    changed function names and added zoom

--- src/components/PictureGrid.js
+++ src/components/PictureGrid.js
@@ -1,19 +1,20 @@
-
 import React from "react";
-import {getAllImages2} from "./downlaodPics";
+import {getAllImages} from "./getData";
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 const PictureGrid = () => {
     const [allUrls, setUrls] = React.useState([])
-    const handleClick = () => getAllImages2().then(urls => setUrls(urls))
+    getAllImages().then(urls => setUrls(urls))
 
     return (
-        <div className="img-grid">
-            images
-            <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mb-3 mt-3">
-                List all songs
-            </button>
+        <div className="mt-3">
             <div className="grid grid-cols-4 gap-4">
-                {allUrls.map(url => <img key={url} src={url} alt={url} className="rounded" />)}
+                {allUrls.map(url =>
+                    <Zoom zoomMargin={10}>
+                        <img key={url} src={url} alt={url} className="rounded"/>
+                    </Zoom>
+                )}
             </div>
         </div>
     )
