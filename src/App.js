d737956cfe26af2f88dbe244385b436d3c3fc86a
laurin <laurin.schwander@outlook.com>
2022-06-02 11:29:57 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Title from './components/Title';
 import './index.css';
 import AlbumSelection from "./components/AlbumSelection"
+import "react-router-dom";
+import Signup from "./components/Signup";
+import Signing from "./components/Signing";
+import Account from "./components/Account";
+import {Routes,Route} from "react-router-dom";
+
 
 function App() {
     return (
         <div className="mr-6 ml-6">
             <Title/>
-            <AlbumSelection/>
+            <Routes>
+                <Route path='/' element={<AlbumSelection/>}/>
+                <Route path='/signup' element={<Signup/>}/>
+                <Route path='/signin' element={<Signing/>}/>
+                <Route path='/account' element={<Account/>}/>
+            </Routes>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 
 import {createRoot} from 'react-dom/client';
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 
 root.render(
-    <Router>
+    <BrowserRouter>
         <App/>
-    </Router>);
+    </BrowserRouter>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
