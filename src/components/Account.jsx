abd909d6ba174e857624af398531a19746061174
laurin <laurin.schwander@outlook.com>
2022-06-09 11:14:49 +0200

    added an album selection

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -30,36 +30,37 @@ const Account = () => {
             console.log(e.message)
         }
     }
-/*
-    const handleChangeEmail = (event) => {
-        event.preventDefault()
-        debugger
-        updateEmail(user, newEmail).then(() => {
-            setNewEmail('')
-        }).catch(error => console.log(error))
-    } */
+    /*
+        const handleChangeEmail = (event) => {
+            event.preventDefault()
+            debugger
+            updateEmail(user, newEmail).then(() => {
+                setNewEmail('')
+            }).catch(error => console.log(error))
+        } */
 
-    return (<div className="max-w-[600px] mx-auto my-4  p-4">
-        <h1 className="text-2xl font-bold py-4">Account</h1>
-        {alert && <div
-            className="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800">
-            <span className="font-medium">Email was sent!</span> We sent you an E-Mail for resetting your
-            password.
-        </div>}
-        <p>User Email: {user && user.email}</p>
-        <div className="container mx-auto">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4">
-                <button onClick={handleLogout}
-                        className="flex justify-center hover:bg-violet-500 bg-violet-600 text-white font-medium py-1 px-2 rounded lg:mb-3 lg:mt-3 overflow-x-auto">
-                    Logout
-                </button>
-                <button onClick={handlePasswordReset}
-                        className="flex justify-center hover:bg-violet-500 bg-violet-600 text-white font-medium py-1 px-2 rounded lg:mb-3 lg:mt-3 overflow-x-auto">
-                    Reset Password
-                </button>
+    return (
+        <div className="max-w-[600px] mx-auto my-4  p-4">
+            <h1 className="text-2xl font-bold py-4">Account</h1>
+            {alert && <div
+                className="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800">
+                <span className="font-medium">Email was sent!</span> We sent you an E-Mail for resetting your
+                password.
+            </div>}
+            <p>User Email: {user && user.email}</p>
+            <div className="container mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4">
+                    <button onClick={handleLogout}
+                            className="flex justify-center hover:bg-violet-500 bg-violet-600 text-white font-medium py-1 px-2 rounded lg:mb-3 lg:mt-3 overflow-x-auto">
+                        Logout
+                    </button>
+                    <button onClick={handlePasswordReset}
+                            className="flex justify-center hover:bg-violet-500 bg-violet-600 text-white font-medium py-1 px-2 rounded lg:mb-3 lg:mt-3 overflow-x-auto">
+                        Reset Password
+                    </button>
+                </div>
             </div>
-        </div>
-    </div>);
+        </div>);
 }
 
 export default Account;
